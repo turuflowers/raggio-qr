bd3acd1717b2cefdb7b819601d13f486a495d6dc
Nicolas Flores <turuflowers@gmail.com>
2022-11-27 09:39:13 -0300

    superior base

--- src/app/scripts/home.js
+++ src/app/scripts/home.js
@@ -7,33 +7,27 @@ wow = new WOW(
 wow.init();
 
 $(document).ready(function() {
+    var $container = $('.especialidades'); //The ID for the list with all the blog posts
+    $container.isotope({
+        // options...
+        itemSelector: '.grid-item',
+        percentPosition: true,
+        masonry: {
+            columnWidth: 0,
+            horizontalOrder: true
+        }
+    });
+
+    $container.isotope( 'layout' );
+    setTimeout( fixbug, 1000 );
+    setTimeout( fixbug, 5000 );
+
+    $(".grid-item button").on('click', function () {
+        $(this).parent().parent().parent().toggleClass('active');
+        $('.especialidades').isotope( 'layout' );
+    });
 });
 
-var countDownDate = new Date("April 16, 2022 17:00:00").getTime();
-var x = setInterval(function() {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-
-    // When the count down is over, write some text
-    if (distance < 0) {
-        clearInterval(x);
-        $('#clock ').html('');
-    }else{
-        // Print the result in html
-        $('#clock .days .numero').html(minTwoDigits(days));
-        $('#clock .hours .numero').html(minTwoDigits(hours));
-        $('#clock .minutes .numero').html(minTwoDigits(minutes));
-        $('#clock .seconds .numero').html(minTwoDigits(seconds));
-    }
-}, 1000);
-
-function minTwoDigits(n) {
-    return (n < 10 ? '0' : '') + n;
+function fixbug() {
+    $('.especialidades').isotope( 'layout' );
 }
